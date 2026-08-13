a81c6167bb3f2cb5ce10717b95fce7a3320da562
arthurveloso1411 <arthurvelosomacambira@gmail.com>
2026-08-13 13:30:47 -0300

    aula2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>hello moto jacare pelad</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bd658e',
     alignItems: 'center',
     justifyContent: 'center',
   },
