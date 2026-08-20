53ac2243f7ec407d2770b41b9082cb8fbfe564f7
arthurveloso1411 <arthurvelosomacambira@gmail.com>
2026-08-20 11:34:20 -0300

    aula 3

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View , Text} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello moto jacare pelad</Text>
-      <StatusBar style="auto" />
+    <View style={BackgroundColor:'pink',flex:1}>
+      <Text></Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#bd658e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
