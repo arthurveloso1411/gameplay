ccc44a3b889f8818b2b4adc96b815b6d44b4ec75
arthurveloso1411 <arthurvelosomacambira@gmail.com>
2026-08-20 13:17:07 -0300

    aula4

--- App.tsx
+++ App.tsx
@@ -2,8 +2,13 @@ import { StyleSheet, View , Text} from 'react-native';
 
 export default function App() {
   return (
-    <View style={BackgroundColor:'pink',flex:1}>
-      <Text></Text>
+    <View style={{backgroundColor:'pink',flex:1}}>
+    <View style={{height:50,width:50,backgroundColor:'orange'}}>
+    </View>
+    <View style={{height:50,width:50,backgroundColor:'brown'}}>
+    </View>
+     <View style={{height:50,width:50,backgroundColor:'blue'}}>
+     </View>
     </View>
   );
 }
